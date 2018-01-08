85fa09adaaccfc09da99d15ad6b528f018698459
Ilia Lataria <kirayshen@gmail.com>
2018-01-08 23:33:23 +0300

    Component for sprite flipping

---
+++ FlipSprite.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSprite : MonoBehaviour
+{
+
+    private bool isFaceRight;
+
+    void Start()
+    {
+        isFaceRight = transform.localScale.x > 0;
+    }
+
+    void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+
+        if (isFaceRight && xInput < 0 || !isFaceRight && xInput > 0)
+        {
+            FlipSpriteProcess();
+            isFaceRight = !isFaceRight;
+        }
+        
+    }
+
+    private void FlipSpriteProcess()
+    {
+        Vector2 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+}
